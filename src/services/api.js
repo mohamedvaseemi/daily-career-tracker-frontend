c30a581d85f21d67a7fe543ad91921a0d8178daf
mohamedvaseemi <vaseem.ismail.mohamed@gmail.com>
2026-08-28 16:31:24 +0530

    frontend is ready to deploy - URL updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Production-la Vercel env variable use pannum, local-la localhost:8080 fallback aagum
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api',
+  baseURL: "https://daily-career-tracker-backend.onrender.com/api" || 'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json'
   },
